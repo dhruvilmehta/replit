25a12b28dd22045b932cad891a520819211be1c1
Dhruvil Mehta <dhruvilmehta2606@gmail.com>
2024-08-10 17:47:07 -0700

    Configured service.yaml domains

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 // const SERVICE_URL = "https://replit.dhruvilspace.site";
 // const SERVICE_URL = "http://localhost:3001";
 const SERVICE_URL = "http://54.242.174.57";
+// const SERVICE_URL = "https://replit.dhruvilspace.site";
 
 export const Login = () => {
   const [username, setUsername] = useState<string>("dhruvil");
